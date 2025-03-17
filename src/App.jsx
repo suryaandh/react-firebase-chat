4711a0f564389165611e94b2f08d28dd435bf5af
suryaandh <gdswiyasa@gmail.com>
2025-03-17 19:57:02 +0700

    feat : chat. detail chat. list chat and user info

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,15 @@
+import List from "./components/list/List";
+import Chat from "./components/chat/Chat";
+import Detail from "./components/detail/Detail";
+
 const App = () => {
-  return <div className="container">App</div>;
+  return (
+    <div className="container">
+      <List />
+      <Chat />
+      <Detail />
+    </div>
+  );
 };
 
 export default App;
---
+++ src/components/list/List.jsx
@@ -0,0 +1,14 @@
+import "./list.css";
+import Chatlist from "./chatList/Chatlist";
+import Userinfo from "./userInfo/Userinfo";
+
+const List = () => {
+  return (
+    <div className="list">
+      <Userinfo />
+      <Chatlist />
+    </div>
+  );
+};
+
+export default List;
